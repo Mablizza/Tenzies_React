87c83a8237d714998cbca94991e273df478f7cda
Julian Arias <mablizza@gmail.com>
2025-02-14 20:06:30 -0600

    Finalizing Commits
    Celebration confetti on Game Won and Resett Game Button Re-Render Conditionally

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react"
 import Die from "./Die"
 import { nanoid } from "nanoid"
+import Confetti from 'react-confetti'
 
 export default function App() {
     
   const [dice, setDice] = useState(generateAllNewDice())
+
+  const gameWon = dice.every(die => die.isHeld) &&
+                  dice.every(die => die.value === dice[0].value)
 
   function generateAllNewDice() {
       return new Array(10)
@@ -14,18 +18,21 @@ export default function App() {
               isHeld: false,
               id: nanoid()
           }))
-  }
+    }
 
   function rollDice() {
-    setDice(oldDice => oldDice.map(die => 
-        die.isHeld ?
-            die :
-            { ...die, value: Math.ceil(Math.random() * 6) }
-    ))
-}
+    if (!gameWon) {
+        setDice(oldDice => oldDice.map(die =>
+            die.isHeld ?
+                die :
+                { ...die, value: Math.ceil(Math.random() * 6) }
+        ))
+    } else {
+        setDice(generateAllNewDice())
+        }
+    }
 
   function hold(id) {
-    // console.log(id)
     setDice(prevDice => prevDice.map( (die) => {
       return die.id == id ? { ...die, isHeld: !die.isHeld } : { ...die }
     }))
@@ -43,10 +50,15 @@ const diceElements = dice.map(dieObj => (
 
   return (
       <main>
-          <div className="dice-container">
-              {diceElements}
-          </div>
-          <button className="roll-dice" onClick={rollDice}>Roll</button>
+        <h1 className="title">Tenzies</h1>
+        <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+        <div className="dice-container">
+            {diceElements}
+        </div>
+        <button className="roll-dice" onClick={rollDice}>
+            {gameWon ? "New Game" : "Roll"}  
+        </button>
+        {gameWon && <Confetti/>}
       </main>
   )
 }
